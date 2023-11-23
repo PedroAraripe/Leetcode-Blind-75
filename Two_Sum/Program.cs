edc18ce15115eb941866afb10ce202c0b2bc281c
Pedro Lucas <pedro.lucx@gmail.com>
2023-11-23 12:16:24 -0300

    refactor : Improving loop readbility TwoSums

--- Two_Sum/Program.cs
+++ Two_Sum/Program.cs
@@ -6,49 +6,40 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+          Console.WriteLine($"\n=====> Quais os indexes do array que somados resultam no valor alvo?");
+
           int[] t1List = {2, 7, 11, 15};
           int t1Target = 9;
+          int[] t1Result = TwoSum(t1List,  t1Target);
+          Console.WriteLine($"{t1Result[0]} e {t1Result[1]}");
 
           int[] t2List = {3, 2, 4};
           int t2Target = 6;
+          int[] t2Result = TwoSum(t2List,  t2Target);
+          Console.WriteLine($"{t2Result[0]} e {t2Result[1]}");
 
           int[] t3List = {3, 3};
           int t3Target = 6;
-
-          Console.WriteLine($"\n=====> Quais os indexes do array que somados resultam no valor alvo?");
-          TwoSum(t1List,  t1Target);
-          TwoSum(t2List,  t2Target);
-          TwoSum(t3List,  t3Target);
+          int[] t3Result = TwoSum(t3List,  t3Target);
+          Console.WriteLine($"{t3Result[0]} e {t3Result[1]}");
         }
 
         static int[] TwoSum(int[] nums, int target) {
             Dictionary<int, int> mapedIntsIndex = new Dictionary<int, int>();
-            int[] indexesToTarget = new int[] { -1, -1 };
-            bool foundIndexes = false;
-
-            int tempIndex = 0;
-
-            while(!foundIndexes && tempIndex < nums.Length) {
-              int currentNum = nums[tempIndex];
-              int sumItemTarget = target - currentNum;
+            
+            for(int i=0; i<nums.Length; i++) {
+              int complement = target - nums[i];
 
               // Searching if we can find the matching number index to sum to target
-              if(mapedIntsIndex.ContainsKey(sumItemTarget)) {
-                indexesToTarget[0] = mapedIntsIndex[sumItemTarget];
-                indexesToTarget[1] = tempIndex;
-
-                foundIndexes = true;
+              if(mapedIntsIndex.ContainsKey(complement)) {
+                return new int[] { mapedIntsIndex[complement],i };
               }
-
+              
               // Populating dictionary with nums and respective indexes
-              mapedIntsIndex.TryAdd(nums[tempIndex], tempIndex);
-              tempIndex++;
+              mapedIntsIndex[nums[i]] = i;
             }
 
-            Console.WriteLine($"\nResultado para o array [ {string.Join(", ", nums)} ] e alvo {target}:");
-            Console.WriteLine($" {indexesToTarget[0]} e {indexesToTarget[1]}  => {nums[indexesToTarget[0]]} + {nums[indexesToTarget[0]]} = {target}");
-
-            return indexesToTarget;
+            return new int[0];
         }
     }
 }
